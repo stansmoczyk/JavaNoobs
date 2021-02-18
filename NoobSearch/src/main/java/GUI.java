79536960c20cd43d78da0147af1973ddf010e8e0
Stan Smoczyk <ssmoczyk@hawkmail.hccfl.edu>
2021-02-18 13:36:46 -0500

    Added label for reference.

--- NoobSearch/src/main/java/GUI.java
+++ NoobSearch/src/main/java/GUI.java
@@ -6,12 +6,14 @@ public class GUI {
         JFrame frame = new JFrame();
 
         JButton button = new JButton("Label goes here");
+        JLabel label = new JLabel("Button clicks: 0");
 
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

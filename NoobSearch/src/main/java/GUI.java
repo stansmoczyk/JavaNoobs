bcb2da4b490c2bbec62d2c8c3c9eee98682f1635
Stan Smoczyk <ssmoczyk@hawkmail.hccfl.edu>
2021-02-18 22:05:12 -0500

    New changes. IDK what they are but hey why not. LOL

--- NoobSearch/src/main/java/GUI.java
+++ NoobSearch/src/main/java/GUI.java
@@ -3,17 +3,21 @@ import java.awt.*;
 
 public class GUI {
     public GUI(){
-        JFrame frame = new JFrame();
+        /*
+         *This code needs more comments to explain what is happening.
+         */
 
-        JButton button = new JButton("Label goes here");
-        JLabel label = new JLabel("Button clicks: 0");
+        JFrame frame = new JFrame(); //Makes new frame Object
+
+        JButton button = new JButton("Label goes here"); //Adds button object
+        JLabel label = new JLabel("Button clicks: 0"); //Adds label object
 
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(label);
+        panel.add(button); //Adds button to frame
+        panel.add(label); //Adds label to frame
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

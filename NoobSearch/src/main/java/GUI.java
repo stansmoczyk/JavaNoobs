77c1612537eff03c458f3cf04025a9e1e7867fd8
yadimar924 <78172441+yadimar924@users.noreply.github.com>
2021-02-21 20:14:43 -0500

    Update GUI.java

--- NoobSearch/src/main/java/GUI.java
+++ NoobSearch/src/main/java/GUI.java
@@ -13,6 +13,9 @@ public class GUI
         JFrame frame = new JFrame();
         JLabel label1 = new JLabel("Welcome to the Noobs Search Engine");
         JTextField search = new JTextField("Type keyword");
+        JRadioButton label2 = new JRadioButton("and");
+        JRadioButton label3 = new JRadioButton("or");
+        JRadioButton label4 = new JRadioButton("phase");
         JButton button = new JButton("Search");
         JButton button1 = new JButton("Feeling lucky");
         JButton button2 = new JButton("About");
@@ -20,9 +23,12 @@ public class GUI
 
 
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(120, 80, 20, 100));
+        panel.setBorder(BorderFactory.createEmptyBorder(80, 150, 20, 150));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(label1);
+        panel.add(label2); //and search
+        panel.add(label3);//or search
+        panel.add(label4);//phase search
         panel.add(search);
         panel.add(button);//Adds Search button
         panel.add(button1);//Adds Feeling lucky button

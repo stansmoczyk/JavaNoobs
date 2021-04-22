f58099436c8157f49582d140eb63435a2e467981
Stan Smoczyk <stansmoczyk@gmail.com>
2021-04-22 12:41:49 -0400

    Formatting for maven and added formatting comment

--- src/main/java/AdminPanel.java
+++ src/main/java/AdminPanel.java
@@ -1,6 +1,11 @@
 //Team members: Joshua Moranville, Joshua Scott, Stan Smoczyk, Yadimar Torres
 //Team name: JavaNoobs
-
+/*Having all the { (curve brackets) to one side helps me see them with my dyslexia.-JoshM.
+ * ie
+ * {
+ * Code in the middle
+ * }
+ * */
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
